69fa2c9dc6713ed20a6a4c2ae15bf41512e86d23
Rahagi Ahnaf Saidani <rahagi@protonmail.com>
2021-09-24 08:54:33 +0700

    init config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { ShipsModule } from './ships/ships.module';
 import { CrawlerModule } from './crawler/crawler.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'schema.gql'),
     }),
+    ConfigModule.forRoot({ isGlobal: true }),
     ShipsModule,
     CrawlerModule,
   ],
